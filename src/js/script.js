8caafe9ba37e5497a4e77c7ee196b06be9c2ec16
maria <mclarasantini@gmail.com>
2026-03-27 09:53:45 -0300

    feat: declarações de variáveis

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+// declarações e variáveis
+//VAR, LET e CONST
+
+var nome = "fiap";
+console.log(nome);
+var nome = "Dev"
+console.log (nome)
+
+if(true){
+    var avatar = "fiapinho"
+}
+
+console.log(avatar);
